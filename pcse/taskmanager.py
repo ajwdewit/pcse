328f21933ab4dbe153c780a4687c83d229b6925a
Allard de Wit <ajwdewit@xs4all.nl>
2014-10-14 09:12:53 +0200

    update docu of taskmanager

--- pcse/taskmanager.py
+++ pcse/taskmanager.py
@@ -8,103 +8,110 @@ Classes defined here:
  TaskManager
 """
 
-from sqlalchemy import *
+import logging
+import sqlalchemy as sa
+from sqlalchemy import select, and_, MetaData, Table
+import socket
 
 class TaskManager:
     """Class defines a taskmanager which reads from a table called 'tasklist'.
 
-    Usage: tm = TaskManager(metadata, connection, dbtype=dbtype,
-                            logger=logger, hostname=hostname)
+    Usage: tm = TaskManager(engine, dbtype=dbtype, tasklist)
 
     Public methods:
     get_task() - picks a 'Pending' task from the list
     set_task_finished(task) - set the task status to 'Finished'
     set_task_error(task) - set the task status to 'Error occurred'
     """
-    
+    validstatus = ['Pending', 'In progress', 'Finished',
+                   'Error occurred']
+    knowndatabases = ['mysql', 'oracle', 'sqlite']
+
 #-------------------------------------------------------------------------------
-    def __init__(self, metadata, connection, dbtype=None, logger=None,
-                 hostname=None, tasklist='tasklist'):
+    def __init__(self, engine, dbtype=None, tasklist='tasklist'):
         """Class constructor for TaskManager.
         
         Arguments:
-        * metadata - An SQLAlchemy database metadata object
-        * connection - An SQLAlchemy database connection object
-        
+        * engine - An SQLAlchemy engine object
+
         Keywords:
-        * dbtype - the type of DB to connect either 'MySQL', 'ORACLE', or 'SQLite'
-        * hostname - The hostname of the machine executing the tasks
+        * dbtype - the type of DB to connect either 'MySQL', 'ORACLE' or 'SQLite'
         * tasklist - Name of table to read tasks from, default 'tasklist'
         """
-        
+        db_ok = False
+        if isinstance(dbtype, str):
+            if dbtype.lower() in self.knowndatabases:
+                db_ok = True
+        if db_ok is False:
+            msg = "keyword 'dbtype' should be one of %s" % self.knowndatabases
+            raise RuntimeError(msg)
+        if not isinstance(engine, sa.engine.base.Engine):
+            msg = "Argument 'engine' should be SQLalchemy database engine, " \
+                  "got %s" % engine
+            raise RuntimeError(msg)
+
         self.dbtype = dbtype.lower()
-        self.connection = connection
-        self.logger = logger
-        self.hostname = hostname
+        self.engine = engine
+        self.logger = logging.getLogger("TaskManager")
+        self.hostname = socket.gethostname()
         self.tasklist_tablename = tasklist
-        self.validstatus = ['Pending', 'In progress', 'Finished',
-                            'Error occurred']
-        self.knowndatabases = ['mysql', 'oracle','sqlite']
-        
-        # Check if database is known
-        if self.dbtype not in self.knowndatabases:
-            errstr =  "Unknown databases type!" +\
-                      "Currently supported databases are: " +\
-                      str(self.knowndatabases)
-            raise RuntimeError(errstr)
+
         # Check if tasklist exists and database is readable
         try:
+            conn = self.engine.connect()
+            metadata = MetaData(conn)
             self.table_tasklist = Table(tasklist, metadata, autoload=True)
-        except Exception, e:
-            raise e("Unable to connect or tasklist table doesn't exist!")
-        
-        
+        except Exception as e:
+            msg = "Unable to connect or tasklist table doesn't exist!"
+            self.logger.exception(msg)
+            raise RuntimeError(msg)
+
 #-------------------------------------------------------------------------------
     def get_task(self):
         "Return 'Pending' task to processing unit."
         
-        self._lock_table()
+        conn = self.engine.connect()
+        self._lock_table(conn)
         tasklist = self.table_tasklist
         s = select([tasklist], and_(tasklist.c.status=='Pending'), 
                    order_by=[tasklist.c.task_id],
                    limit=1)
-        r = self.connection.execute(s)
+        r = conn.execute(s)
         row = r.fetchone()
         r.close()
         if row is None:
-            self._unlock_table()
+            self._unlock_table(conn)
             return None
         else:
             task = dict(row)
             u = tasklist.update(tasklist.c.task_id==task["task_id"])
-            self.connection.execute(u, status='In progress',
-                                    hostname=self.hostname)
-        self._unlock_table()
+            conn.execute(u, status='In progress',  hostname=self.hostname)
+        self._unlock_table(conn)
         return task
     
 #-------------------------------------------------------------------------------
-    def _lock_table(self):
+    def _lock_table(self, connection):
         """Locks the TASKLIST table. Note that locking/unlocking is carried out
         by directly sending SQL to the database instead of using SQLAlchemy,
         which does not support this kind of table locking."""
     
         if self.dbtype=="mysql":
-            self.connection.execute("LOCK TABLE %s WRITE" % self.tasklist_tablename) 
+            connection.execute("LOCK TABLE %s WRITE" % self.tasklist_tablename)
         elif self.dbtype=="oracle":
-            self.connection.execute("LOCK TABLE %s IN EXCLUSIVE MODE" % \
-                                    self.tasklist_tablename)
+            connection.execute("LOCK TABLE %s IN EXCLUSIVE MODE" %
+                               self.tasklist_tablename)
         elif self.dbtype=="sqlite":
             pass # No locking needed for SQLite: assuming one client only.
         
 
 #-------------------------------------------------------------------------------
-    def _unlock_table(self):
+    def _unlock_table(self, connection):
         "Unlocks the TASKLIST table"
     
         if self.dbtype=="mysql":
-            self.connection.execute("UNLOCK TABLES")
+            connection.execute("UNLOCK TABLES")
         elif self.dbtype=="oracle":
-            self.connection.execute("COMMIT")
+            connection.execute("COMMIT")
         elif self.dbtype=="sqlite":
             pass # No locking needed for SQLite: assuming one client only.
         
@@ -112,16 +119,18 @@ class TaskManager:
     def set_task_finished(self, task):
         "Sets a task to status 'Finished'"
         
-        self._lock_table()
+        conn = self.engine.connect()
+        self._lock_table(conn)
         u = self.table_tasklist.update(self.table_tasklist.c.task_id==task["task_id"])
-        self.connection.execute(u, status='Finished')
-        self._unlock_table()
+        conn.execute(u, status='Finished')
+        self._unlock_table(conn)
         
 #-------------------------------------------------------------------------------
     def set_task_error(self, task):
         "Sets a task to status 'Error occurred'"
         
-        self._lock_table()
+        conn = self.engine.connect()
+        self._lock_table(conn)
         u = self.table_tasklist.update(self.table_tasklist.c.task_id==task["task_id"])
-        self.connection.execute(u, status='Error occurred')
-        self._unlock_table()
+        conn.execute(u, status='Error occurred')
+        self._unlock_table(conn)
