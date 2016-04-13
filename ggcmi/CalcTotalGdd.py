101febe456551fd0c2e8a2afe6b3f69622548b6e
hoek008 <steven.hoek@wur.nl>
2016-04-13 16:01:03 +0200

    These files were changed: now depending on resp. containing class CropInfoProvider

--- ggcmi/CalcTotalGdd.py
+++ ggcmi/CalcTotalGdd.py
@@ -1,75 +1,155 @@
-import pcse;
-from CropInfoReader import CropInfoProvider;
+import sys, os
+sys.path.append(r"D:\UserData\hoek008\GGCMI\PySrc\pcse")
+
+# import pcse;
+from cropinforeader import CropInfoProvider;
 #from pcse.pcse.models import Wofost71_PP;
-from pcse.pcse.engine import Engine
-from AgmerraNetcdfReader import AgmerraWeatherDataProvider;
-from numpy import arange;  
+from pcse.base_classes import WeatherDataProvider
+from pcse.engine import Engine
+from pcse.fileinput.hdf5reader import Hdf5WeatherDataProvider
+#from AgmerraNetcdfReader import AgmerraWeatherDataProvider;
+from numpy import arange, mean, std;  
+from datetime import datetime, date;
+import sqlite3;
+import time
+import logging
+# import pdb;
 
 def main():
-    # Loop over the crops
-    watersupplies = ["rf", "ir"];
-    datadir = r"D:\UserData\hoek008\GGCMI\phase1\data"
-    cropdict = CropInfoProvider.getCrops();
-    for crop in cropdict:
-        # Loop over the 2 water supply options
-        for watersupply in watersupplies:
-            cropname = cropdict[crop];
-            cip = CropInfoProvider(cropname, watersupply, datadir);
-            nvlp = cip.getExtent();
-            cropdata = cip.getCropData();
-            
-            # Make sure we can loop over the grid cells from UL down to LR corner
-            x_range = arange(nvlp.getMinX() + 0.5*nvlp.dx, nvlp.getMaxX() + 0.5*nvlp.dy, nvlp.dx);
-            y_range = arange(nvlp.getMinY() + 0.5*nvlp.dy, nvlp.getMaxY() + 0.5*nvlp.dx, nvlp.dy);
-            if (nvlp.xcoords_sort != 'ASC'): x_range = reversed(x_range);
-            if (nvlp.ycoords_sort != 'DESC'): y_range = reversed(y_range);
-            
-            # Loop but make sure not to waste time on pixels that are not interesting
-            for lat in y_range:
-                for lon in x_range: 
-                    # Check the landmask
-                    if not cip.get_landmask(lon, lat):
-                        continue;
-                    
-                    # Check the data on the growing season; start_day eq.to -99 means that area is
-                    # usu. only little above crop_specific base temperature and end_day equal to -99
-                    # means that hot periods need to be avoided - 
-                    start_day, end_day = cip.getSeasonDates(lon, lat);
-                    if (start_day == -99) or (end_day == -99):
-                        continue;
-    
-                    # Initialise
-                    wdp = None
-                    try: 
-                        # Retrieve the relevant weather data
-                        wdp = AgmerraWeatherDataProvider(lon, lat, datadir);
+    # Initialise
+    datadir = r"D:/UserData/hoek008/GGCMI/phase1/data/"
+    db_location = os.path.join(datadir, "ggcmi.db");
+    conn = None;
+    try:
+        conn = sqlite3.connect(db_location);
+    except Exception as e:
+        print "SQLite: " + str(e);
+    if conn == None: exit(1);
+    remotedir = r"Z:/projects/ggcmi/data/AgMERRA"
+    fn = r"AgMERRA_1980-01-01_2010-12-31f.hf5"
+
+    try:
+        # Loop over the crops
+        watersupplies = ["rf", "ir"];
+        cropdict = CropInfoProvider.getCrops();
+        for crop in cropdict:
+            # Loop over the 2 water supply options
+            for watersupply in watersupplies:
+                # Get the crop name and insert a new record into the crop table
+                cropname = cropdict[crop];
+                crop_no = insert_crop(conn, cropname, watersupply);
+                
+                # Get a crop info provider'
+                t1 = time.time()
+                cip = CropInfoProvider(cropname, watersupply, datadir);
+                nvlp = cip.getExtent();
+                cropdata = cip.getCropData();
+                msg = ("Retrieving data for crop %s, %s took %6.1f seconds" % (cropname, watersupply, time.time()-t1))
+                print msg;
+                
+                # Make sure we can loop over the grid cells from UL down to LR corner
+                x_range = arange(nvlp.getMinX() + 0.5*nvlp.dx, nvlp.getMaxX() + 0.5*nvlp.dy, nvlp.dx);
+                y_range = arange(nvlp.getMinY() + 0.5*nvlp.dy, nvlp.getMaxY() + 0.5*nvlp.dx, nvlp.dy);
+                if (nvlp.xcoords_sort != 'ASC'): x_range = reversed(x_range);
+                if (nvlp.ycoords_sort != 'DESC'): y_range = reversed(y_range);
+                
+                # Loop but make sure not to waste time on pixels that are not interesting
+                for lat in y_range:
+                    for lon in x_range: 
+                        # Check the landmask
+                        if not cip.get_landmask(lon, lat):
+                            continue;
                         
-                        # Loop over the years
-                        for year in wdp.available_years:
-                            try:
-                                # Get timer data for the current year
-                                timerdata = cip.getTimerData(start_day, end_day, year);
-                                sitedata = None;
-                                soildata = None;
+                        # Check the data on the growing season; start_day eq.to -99 means that area is
+                        # usu. only little above crop_specific base temperature and end_day equal to -99
+                        # means that hot periods need to be avoided - 
+                        start_day, end_day = cip.getSeasonDates(lon, lat);
+                        if (start_day == -99) or (end_day == -99):
+                            continue;
+        
+                        # Initialise
+                        wdp = None
+                        try: 
+                            # Retrieve the relevant weather data
+                            wdp = Hdf5WeatherDataProvider(fn, lat, lon, remotedir);
                             
-                                # Run simulation
-                                #wofsim = Wofost71_PP(sitedata, timerdata, soildata, cropdata, wdp);
-                                pheno = Engine(sitedata, timerdata, soildata, cropdata, wdp, config="Wofost71_PhenoOnly.conf")
-                                pheno.run(days=366)
-                                results = pheno.get_output()
-                                sumresults = pheno.get_summary_output()
+                            # Loop over the years
+                            t2 = time.time()
+                            tsums = [];
+                            for year in get_available_years(wdp):
+                                try:
+                                    # Get timer data for the current year
+                                    timerdata = cip.getTimerData(start_day, end_day, year);
+                                    sitedata = {};
+                                    soildata = {"SMFCF":0.4};
                                 
-                                # Store result
-                                grid_no = wdp.get_grid_no(lon, lat);
-                            except Exception, e:
-                                print str(e);   
-                    except Exception, e:
-                        print str(e);                                    
-                    finally:
-                        if (wdp != None): 
-                            wdp.close();
+                                    # Run simulation
+                                    pheno = Engine(sitedata, timerdata, soildata, cropdata, wdp, config="Wofost71_PhenoOnly.conf")
+                                    pheno.run(days=366)
+                                    
+                                    # Retrieve result
+                                    results = pheno.get_output()
+                                    if (results != None) and isinstance(results, list):
+                                        print results[-1];
+                                    sumresults = pheno.get_summary_output();
+                                    print sumresults;
+                                    if isinstance(sumresults[0], dict) and isinstance(sumresults[0]["TSUM"], float):
+                                        tsums.append(sumresults[0]["TSUM"]);
+                                except Exception, e:
+                                    print str(e);
+                                # end try  
+                            # end year 
+                            
+                            # Insert average etc. into database
+                            if len(tsums) > 0:
+                                insert_tsum(conn, 1, crop_no, lat, lon, mean(tsums), std(tsums, ddof=1), min(tsums), max(tsums), len(tsums));
+                            msg = ("Simulating for crop %s, %s took %6.1f seconds" % (cropname, watersupply, time.time()-t2)) 
+                            print msg;
+                            exit();
+                            
+                        except Exception, e:
+                            print str(e);                                    
+                        finally:
+                            if (wdp != None): 
+                                wdp.close();
+                        # end try        
+                    # end lon            
+                # end lat     
+                cip.close();
+            # end watersupply
+        # end crop        
+    finally:
+        conn.close();
+
+def get_available_years(wdp):
+    result = []
+    if isinstance(wdp, WeatherDataProvider):
+        tmpList = [wdp.first_date.year, wdp.last_date.year];
+        if wdp.first_date != datetime(wdp.first_date.year, 1, 1).date():
+            tmpList[0] = wdp.first_date.year + 1;
+        if wdp.last_date != datetime(wdp.last_date.year, 12, 31).date():
+            tmpList[1] = wdp.last_date.year - 1;
+        result = range(tmpList[0], tmpList[1]);
+    return result;
+
+def insert_crop(conn, crop_name, mgmt_code):
+    # TODO: this is prone to SQL injection - insecure!
+    strSql = "INSERT INTO 'crop' VALUES (NULL, '%s', '%s')" % (crop_name, mgmt_code);
+    cursor = conn.cursor();
+    cursor.execute(strSql)
+    conn.commit();
+    result = cursor.lastrowid;
+    del cursor;
+    return result;
     
-            cip.close();
-    
+def insert_tsum(conn, dataset_id, crop_no, lat, lon, avg, stdev, minval, maxval, numobs):
+    # TODO: this is prone to SQL injection - insecure!
+    strSql = "INSERT INTO 'tsum' VALUES (%i, %02i, %3.2f, %3.2f, %3.2f, %3.2f, %3.2f, %3.2f, %02i)";
+    strSql = strSql % (dataset_id, crop_no, lat, lon, avg, stdev, minval, maxval, numobs);
+    cursor = conn.cursor();
+    cursor.execute(strSql)
+    conn.commit();
+    del cursor;
+        
 if __name__ == '__main__':
     main();
--- ggcmi/CropInfoReader.py
+++ ggcmi/CropInfoReader.py
@@ -1,182 +1,164 @@
-import os;
-from pcse.pcse.geo import GridEnvelope2D, AsciiGrid;
-from netCDF4 import Dataset;
-from pcse.pcse.fileinput import CABOFileReader;
-from pcse.pcse.netcdf4 import NetcdfEnvelope2D;
-from pcse.pcse.exceptions import PCSEError;
-from datetime import datetime, date;
-from numpy import ma;
+import os
+from pcse.exceptions import PCSEError
+from netCDF4 import Dataset
+from pcse.fileinput import CABOFileReader
+from pcse.geo.floatingpointraster import FloatingPointRaster
+from pcse.geo.netcdf4envelope2d import Netcdf4Envelope2D
+from datetime import date, timedelta
+import numpy as np
+
+import run_settings
 
 class CropInfoProvider():
-    regions = {"Europe": GridEnvelope2D(70, 136, -11., 36., 0.5, 0.5),
-               "World": GridEnvelope2D(360, 720, -180., -90., 0.5, 0.5),
-               "Tropics": GridEnvelope2D(94, 720, -180., -23.5, 0.5, 0.5)};
-    
+    regions = run_settings.regions
+
     # CABO name - filename
     # TODO add more elements for different continents ???
-    _crop_info_sources = [("Barley", "World", "BAR301.CAB", 1),
-            ("Cassava", "World", "CASSAVA.W41", 2),
-            ("Groundnuts", "World", "GR_NUT.W41", 1),
-            ("Maize", "Europe", "MAG203.CAB", 1), 
-            ("Millet", "World", "MILLET.W41", 1),
-            ("Potatoes", "World",  "POT702.CAB", 2),
-            ("Pulses", "World", "PIGEOPEA.W41", 1),
-            ("Rapeseed", "World", "RAP1002.CAB", 1),
-            ("Rice", "World", "RIC501.CAB", 1),
-            ("Rye", "World", "RYE.W41", 1),
-            ("Sorghum", "World", "SORGHUM.W41", 1),
-            ("Soybeans", "World", "SOYBEAN.W41", 1),
-            ("Sunflower", "World", "SUN1101.CAB", 1),
-            ("Wheat", "World", "WWH105.CAB", 1)
-            ];
-            # ("Maize", "Tropics", "MAIZE.W41", 1),
-            
-    # File - path is relative to files in subdirectory OtherInputs\GrowingSeason_vs1_24
-    landmask_grid = r"../../geodata/glob_landmask_resampled.flt";
-    
+    _crop_info_sources = run_settings.crop_info_sources 
+
+    # File - path to *.flt file with land mask
+    landmask_grid = run_settings.landmask_grid
+
     # Netcdf dataset with planting and harvest dates
-    _ds = None;
-    _cropdata = None;    
-    _envelope = None;
-    
+    _ds = None
+    _cropdata = None
+    _envelope = None
+
     def __init__(self, crop, watersupply="rf", fpath=None):
         # match the given crop and region with a tuple from the list
-        _fn = self._crop_info_sources[0][2];
-        for cropname, region, filename, group_no in self._crop_info_sources:
+        _fn = self._crop_info_sources[0][2]
+        for cropname, _, filename, _ in self._crop_info_sources:
             # for the mean time, don't worry about the region
             if cropname == crop:
-                _fn = filename;
-                break;
-        
+                _fn = filename
+                break
+
         # read parameters from the filename stored in _crop_info_sources
-        _fn = os.path.join(fpath, "OtherInputs", 'CROPD', _fn);
-        self._cropdata = CABOFileReader(_fn);
-    
+        _fn = os.path.join(fpath, run_settings.crop_input_folder, run_settings.cabofile_folder, _fn)
+        self._cropdata = CABOFileReader(_fn)
+
         # Prepare to read planting and harvest data from the netcdf file
-        _fn = crop + "_" + watersupply + "_growing_season_dates_v1.24.nc4";
-        _fn = os.path.join(fpath, "OtherInputs", "GrowingSeason_vs1_24", _fn);
+        _fn = crop + "_" + watersupply + run_settings.growing_season_file_suffix
+        _fn = os.path.join(fpath, run_settings.crop_input_folder, run_settings.growing_season_folder, _fn)
 
         try:
             # Open the file
-            self._ds = Dataset(_fn, 'r');
-            self._envelope = NetcdfEnvelope2D.getEnvelope(self._ds);
+            self._ds = Dataset(_fn, 'r')
+            self._envelope = Netcdf4Envelope2D(self._ds)
         except Exception as e:
-            fn = os.path.basename(self._ds.filepath());
-            raise PCSEError("An error occurred while opening file " + fn + " (" + str(e) + ")");
-    
+            fn = os.path.basename(self._ds.filepath())
+            raise PCSEError("An error occurred while opening file " + fn + " (" + str(e) + ")")
+
     @staticmethod
     def getCropGroup(aCropName):
-        result = 0;
-        for cropname, region, filename, group_no in CropInfoProvider._crop_info_sources:
+        result = 0
+        for cropname, _, _, group_no in CropInfoProvider._crop_info_sources:
             if cropname == aCropName:
-                result = group_no;
-                break;
-        return result;
-    
+                result = group_no
+                break
+        return result
+
     @staticmethod
     def getCrops():
-        result = {};
-        i = 1;
-        for cropname, region, filename, group_no in CropInfoProvider._crop_info_sources:
-            result[i] = cropname;
-            i = i + 1;
-        return result;
-    
+        result = {}
+        i = 1
+        for cropname, _, _, _ in CropInfoProvider.self._ds_crop_info_sources:
+            result[i] = cropname
+            i = i + 1
+        return result
+
     def getCropData(self):
-        return self._cropdata;   
-    
+        return self._cropdata
+
     def getSeasonDates(self, longitude, latitude):
-        start_day = -99;
-        end_day = -99;
+        eps = 0.001
+
+        # Check that the netCDF file is loaded
+        if self._ds is None:
+            raise PCSEError("file is no more open.")
+
+        # Check that the given latitude and longitude is within extent of the netCDF4 file
+        if not self._envelope.isWithinExtent(longitude, latitude):
+            raise PCSEError("Given lat-lon coordinates are beyond the borders of the file.")
+        k, i = self._envelope.getColAndRowIndex(longitude, latitude)
+
+        # Check that the found indices are really linked to the given lat-lon
+        msg = " coordinate for this index not as expected: "
+        assert abs(self._ds.variables["lat"][i] - latitude) < 0.5*self._envelope.dy + eps, "Y" + msg + str(i)
+        assert abs(self._ds.variables["lon"][k] - longitude) < 0.5*self._envelope.dx + eps, "X" + msg + str(k)
+
+        # Take into account that the variables are made out of masked arrays
+        start_doy = -99
+        end_doy = -99
         try:
-            # Check that the netCDF file is loaded
-            if self._ds == None: raise PCSEError("file is no more open.");
-            
-            # Check that the given latitude and longitude is within extent of the netCDF4 file
-            if not self._envelope.isWithinExtent(longitude, latitude):
-                raise PCSEError("Given lat-lon coordinates are beyond the borders of the file.");
-            k, i = self._envelope.getColAndRowIndex(longitude, latitude);
-            
-            # Check that the found indices are really linked to the given lat-lon
-            LON = NetcdfEnvelope2D.LON;
-            LAT = NetcdfEnvelope2D.LAT;
-            msg = " coordinate for this index not as expected: "
-            assert self._ds.variables[LAT][i] == latitude, "Y" + msg + str(i);
-            assert self._ds.variables[LON][k] == longitude, "X" + msg + str(k);
-            
-            # Look for the variables indicating the season
-            planting_day = 'planting day';
-            harvest_day  = 'harvest day';
-            if (planting_day in self._ds.variables) and (harvest_day in self._ds.variables):
-                # Take into account that the variables are made out of masked arrays
-                arr_elem = self._ds.variables[planting_day][i, k];
-                if str(arr_elem) != '--': start_day = int(arr_elem);
-                arr_elem = self._ds.variables[harvest_day][i, k];
-                if str(arr_elem) != '--': end_day = int(arr_elem);
-            return start_day, end_day;
-        except Exception as e:
-            fn = os.path.basename(self._ds.filepath());
-            raise PCSEError("An error occurred while reading file " + fn + " (" + str(e) + ")");
-            
-    def getTimerData(self, start_day, end_day, year): 
-        result = None;
+            arr_elem = self._ds.variables['planting day'][i, k]
+            start_doy = int(arr_elem)
+            if start_doy < 0: start_doy = -99
+            arr_elem = self._ds.variables['harvest day'][i, k]
+            end_doy = int(arr_elem)
+            if end_doy < 0: end_doy = -99
+        except np.ma.core.MaskError:
+            pass
+        return start_doy, end_doy
+
+
+    def getTimerData(self, start_doy, end_doy, year):
+
         try:
             # Prepare the timer data
-            result = {};
-            result['CAMPAIGNYEAR'] = year;
-            if end_day > start_day:
+            result = {}
+            if end_doy > start_doy:
                 # Assume that start and end time are within the same year
-                crop_end_date = datetime.strptime(str(year) + ' ' + str(end_day), '%Y %j').date();
-                result['START_DATE'] = date(year, 1, 1);
-                result['END_DATE'] = date(year, 12, 31);
+                crop_start_date = date(year,1,1) + timedelta(days=start_doy-1)
+                crop_end_date = date(year,1,1) + timedelta(days=end_doy-1)
             else:
-                # Not within 1 year; try to start the season 90 days before the crop start date
-                crop_end_date = datetime.strptime(str(year + 1) + ' ' + str(end_day), '%Y %j').date();
-                tmpday = max(end_day, start_day - 90);
-                result['START_DATE'] = datetime.strptime(str(year) + ' ' + str(tmpday), '%Y %j').date();
-                result['END_DATE'] = datetime.strptime(str(year + 1) + ' ' + str(tmpday-1), '%Y %j').date();
-            crop_start_date = datetime.strptime(str(year) + ' ' + str(start_day), '%Y %j').date();
-            result['CROP_START_DATE'] = crop_start_date;        
-            result['CROP_END_DATE'] = crop_end_date     
-            result['CROP_START_TYPE'] = 'sowing';
-            result['CROP_END_TYPE'] = 'harvest'; 
-            result['MAX_DURATION'] = 300;
-            return result;
+                # Not within 1 year
+                crop_start_date = date(year,1,1) + timedelta(days=start_doy-1)
+                crop_end_date = date(year+1,1,1) + timedelta(days=end_doy-1)
+            result['START_DATE'] = crop_start_date
+            result['END_DATE'] = crop_end_date
+            result['CROP_START_DATE'] = crop_start_date
+            result['CROP_END_DATE'] = crop_end_date
+            result['CAMPAIGNYEAR'] = year
+            result['CROP_START_TYPE'] = 'sowing'
+            result['CROP_END_TYPE'] = 'harvest'
+            result['MAX_DURATION'] = 365
+            return result
         except Exception as e:
-            raise PCSEError("An error occurred while preparing the timer data: " + str(e));
-        
+            raise PCSEError("An error occurred while preparing the timer data: " + str(e))
+
     def close(self):
-        if self._ds != None: 
-            self._ds.close();
-            self._ds = None;
-            
+        if self._ds != None:
+            self._ds.close()
+            self._ds = None
+
     def getExtent(self):
-        return self._envelope;
-        
+        return self._envelope
+
     def _getFullPath(self, fname):
-        path = os.path.dirname(self._ds.filepath());
-        result = os.path.join(path, fname);
-        result = os.path.normpath(result);
-        return result;
-    
+        path = os.path.dirname(self._ds.filepath())
+        result = os.path.join(path, fname)
+        result = os.path.normpath(result)
+        return result
+
     def _get_value_from_grid(self, longitude, latitude, fpath):
         # Open the file. Get right row and column. Elevations are linked to the cell centres
-        r = AsciiGrid(fpath, "i");
-        if not r.open('r'): raise Exception("Unable to open input file " + r.name);
-        k, i = r.getColAndRowIndex(longitude, latitude);
-        if (i == r.nrows): i = i - 1;
-        
+        r = FloatingPointRaster(fpath, "i")
+        if not r.open('r'): raise Exception("Unable to open input file " + r.name)
+        k, i = r.getColAndRowIndex(longitude, latitude)
+        if (i == r.nrows): i = i - 1
+
         # Now get hold of the right row, read the wanted value and close
-        for _ in range(0, i): r.next(False);
-        line = r.next(); # this line is split, unlike the previous ones
-        r.close();
-        if (k == r.ncols): k = k - 1;
-        return line[int(k)];   
-    
+        for _ in range(0, i+1):
+            rawline = r.next()
+        r.close()
+        if (k == r.ncols): k = k - 1
+        line = np.frombuffer(rawline, 'f')
+        return line[int(k)]
+
     def get_landmask(self, longitude, latitude):
-        fpath = self._getFullPath(self.landmask_grid);
-        value = self._get_value_from_grid(longitude, latitude, fpath);
-        return (value == 1);
-    
-    
-            
+        fpath = self._getFullPath(self.landmask_grid)
+        value = self._get_value_from_grid(longitude, latitude, fpath)
+        return (value == 1)
+
+
