a7eb583f39c55dc940361f3b314310666123ec60
hoek008 <steven.hoek@wur.nl>
2016-04-14 16:32:56 +0200

    The files from the geo package were moved to a separate repository called lmgeo on Github.com

--- pcse/geo/__init__.py
+++
@@ -1,6 +0,0 @@
-from .asciigrid import AsciiGrid;
-from .floatingpointraster import FloatingPointRaster;
-from .gridenvelope2d import GridEnvelope2D;
-from .netcdf4envelope2d import Netcdf4Envelope2D;
-from .hdf5raster import Hdf5Raster;
-from .netcdf4raster import Netcdf4Raster;
