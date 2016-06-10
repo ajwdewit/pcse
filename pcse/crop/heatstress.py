81e9de108ef9a79388721fa961215e185cb07ae3
Iwan Supit <iwan.supit@wur.nl>
2016-06-10 10:38:14 +0200

    correct conceptual errors now the correct ones

--- pcse/crop/heatstress.py
+++ pcse/crop/heatstress.py
@@ -1,40 +1,53 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2004-2014 Alterra, Wageningen-UR
-# Allard de Wit (allard.dewit@wur.nl), April 2014
-# Adapted for translocation, sink limitation and
-# heat stress by Iwan Supit (iwan.supit@wur.nl), June 2016
+# Copyright (c) 2004-2015 Alterra, Wageningen-UR
+# Allard de Wit (allard.dewit@wur.nl )and Iwan Supit
+# (iwan.supit@wur.nl), June 2016
+# Approach based on LINTUL4-vsht made by Joost Wolf
 
-from ..traitlets import Float, AfgenTrait
+"""Implementation of a models for heatstress around flowering in WOFOST
+
+Classes defined here:
+- WOFOST_Sink_Dynamics:
+"""
+import datetime
+
+from ..traitlets import Float, Int, Instance, Enum, Bool, AfgenTrait
 from ..decorators import prepare_rates, prepare_states
+
 from ..base_classes import ParamTemplate, StatesTemplate, RatesTemplate, \
     SimulationObject, VariableKiosk
+from .. import exceptions as exc
 
 
-class WOFOST_Storage_Organ_Dynamics(SimulationObject):
-    """Implementation of storage organ dynamics.
+# -------------------------------------------------------------------------------
+class WOFOST_Sink_Dynamics(SimulationObject):
+    """Implements the algorithm for heatstress around flowering in WOFOST.
+     1/ The number of sinks is established as function of the total leaf and
+        stem weight at anthesis.
+     2/ The number of sinks is reduced by a factor for high temperatures. This
+        factor depends on the average day time temperature during the sensitive
+        period.
+     3/ The number of sinks is also reduced by low temperature at the end of the
+        sensitive period. This reduction factor depends on the day time temperature
+        at DVSHEF.
 
-    Storage organs are the most simple component of the plant in WOFOST and
-    consist of a static pool of biomass. Growth of the storage organs is the
-    result of assimilate partitioning. Death of storage organs is not
-    implemented and the corresponding rate variable (DRSO) is always set to
-    zero.
-
-    Pods are green elements of the plant canopy and can as such contribute
-    to the total photosynthetic active area. This is expressed as the Pod
-    Area Index which is obtained by multiplying pod biomass with a fixed
-    Specific Pod Area (SPA).
 
     **Simulation parameters**
 
     =======  ============================================= =======  ============
      Name     Description                                   Type     Unit
     =======  ============================================= =======  ============
-    TDWI     Initial total crop dry weight                  SCr      |kg ha-1|
-    SPA      Specific Pod Area                              SCr      |ha kg-1|
-    GRMX     Maximal grain mass one grain                   SCr      |kg|
-    PGRIG    Potential grain formation per grain            SCr      |kg|
-    TMGTB    Reduction factor of grain formation  as
-             function. Values from SWHEAT model
+    RDGRTB   Reduction factor of grain formation due        TCr         -
+             to heat stress in the period around anthesis   TCr         -
+
+    DVSHEB   Beginning of the period with possible temp     SCr        |day|
+             sensitivity effects
+    DVSHEF   End of the period with possible heat temp    SCr        |day|
+             sensitivity effects
+    NUMGA    Variable A in relation between TAGP at         SCr        |ha-1|
+             anthesis and sink dimension
+    NUMGB    Variable B in relation between TAGP at         SCr        |ha-1|
+             anthesis and sink dimension
     =======  ============================================= =======  ============
 
     **State variables**
@@ -42,10 +55,16 @@ class WOFOST_Storage_Organ_Dynamics(SimulationObject):
     =======  ================================================= ==== ============
      Name     Description                                      Pbl      Unit
     =======  ================================================= ==== ============
-    PAI      Pod Area Index                                     Y     -
-    WSO      Weight of living storage organs                    Y     |kg ha-1|
-    DWSO     Weight of dead storage organs                      N     |kg ha-1|
-    TWSO     Total weight of storage organs                     Y     |kg ha-1|
+    CMDTEMP  Temperature sum day-time temperature               N    |C| day
+             between DVSHEB and DVSHEF
+    DSB      Starting day of heat sensitivity                   N    -
+    DSE      End day of heat sensitivity                        N    -
+    HSTAGE   Current phenological stage, can take the           N    -
+             folowing values:
+             `not sensitive|temp sensitive|end temp sensitivty`
+    NUMGR    Number of grains                                   Y    -
+    TAGBSF   Total leaf and stem weight (dead/alive)           N    |kg ha-1|
+             at anthesis
     =======  ================================================= ==== ============
 
     **Rate variables**
@@ -53,120 +72,151 @@ class WOFOST_Storage_Organ_Dynamics(SimulationObject):
     =======  ================================================= ==== ============
      Name     Description                                      Pbl      Unit
     =======  ================================================= ==== ============
-    GRSO     Growth rate storage organs                         N   |kg ha-1 d-1|
-    DRSO     Death rate storage organs                          N   |kg ha-1 d-1|
-    GWSO     Net change in storage organ biomass                N   |kg ha-1 d-1|
-    TRANSL   Weight that is translocated to storage organs      N   |kg ha-1 d-1|
+    DAYTEMP  Daily increase of day-time temperature             N       |C|
     =======  ================================================= ==== ============
 
-    **Signals send or handled**
+    **External dependencies:**
 
-    None
-
-    **External dependencies**
 
     =======  =================================== =================  ============
      Name     Description                         Provided by         Unit
     =======  =================================== =================  ============
-    ADMI     Above-ground dry matter             CropSimulation     |kg ha-1 d-1|
-             increase
-    FO       Fraction biomass to storage organs  DVS_Partitioning    -
-    FR       Fraction biomass to roots           DVS_Partitioning    -
+    DVS      Crop development stage              DVS_Phenology       -
+    TWLV     Total weight of leaves              leaf_dynamics       |kg ha-1|
+    TWST     Total weight of stems               stem_dynamics       |kg ha-1|
     =======  =================================== =================  ============
+
     """
 
     class Parameters(ParamTemplate):
-        SPA = Float(-99.)
-        TDWI = Float(-99.)
-        ISINK = Float(-99.)
-        PGRIG = Float(-99.)
-        GRMX = Float(-99.)
-        TMGTB = AfgenTrait()  # Low temperature response function for grain formation as a function of day-time temp.
+        DVSHEB = Float(-99.)  # beginning of the period with possible heat stress effects.
+        DVSHEF = Float(-99.)  # finalization of the period with possible heat stress effects.
+        IHEAT = Float(-99.)  # Switch for heat sensitivity of sink.
+        NUMGA = Float(-99.)  # Variable A in relation between TAGP at anthesis anthesis and sink dimension.
+        NUMGB = Float(-99.)  # Variable B in relation between TAGP at anthesis anthesis and sink dimension.
+        RDGRTB = AfgenTrait()  # High temperature response function for grain formation as a function of day-time temp.
 
-    class StateVariables(StatesTemplate):
-        WSO = Float(-99.)  # Weight living storage organs
-        DWSO = Float(-99.)  # Weight dead storage organs
-        TWSO = Float(-99.)  # Total weight storage organs
-        PAI = Float(-99.)  # Pod Area Index
-
+    # -------------------------------------------------------------------------------
     class RateVariables(RatesTemplate):
-        GRSO = Float(-99.)
-        DRSO = Float(-99.)
-        GWSO = Float(-99.)
+        DAYTEMP = Float(-99.)  # Increase in day-time temperature sum.
 
+    # -------------------------------------------------------------------------------
+    class StateVariables(StatesTemplate):
+        CMDTEMP = Float(-99.)  # Sum of the day-time temperature.
+        TAGBSF = Float(-99.)  # Total weight of leaves and stems at anthesis.
+        NUMGR = Float(-99.)  # number of sinks (per ha).
+
+        # States which register phenological events
+        DSB = Instance(datetime.date)  # Day where temp sensitivity begins.
+        DSE = Instance(datetime.date)  # Day where temp sensitivity ends.
+
+        HSTAGE = Enum([None, "not sensitive", "temp sensitive", "end temp sensitivity"])
+
+    # ---------------------------------------------------------------------------
     def initialize(self, day, kiosk, parvalues):
-        """
-        :param day: start date of the simulation
-        :param kiosk: variable kiosk of this PCSE  instance
-        :param parvalues: `ParameterProvider` object providing parameters as
-                key/value pairs
-        """
 
+        HSTAGE = "not sensitive"
+        DSB = None
+        DSE = None
+
+        self.rates = self.RateVariables(kiosk, publish="DAYTEMP")
         self.params = self.Parameters(parvalues)
-        self.rates = self.RateVariables(kiosk)
-        self.kiosk = kiosk
+        self.states = self.StateVariables(kiosk, publish="NUMGR", NUMGR=0, TAGBSF=0,
+                                          CMDTEMP=0., DSB=DSB, DSE=DSE, HSTAGE=HSTAGE)
 
-        # INITIAL STATES
-        params = self.params
-        # Initial storage organ biomass
-        FO = self.kiosk["FO"]
-        FR = self.kiosk["FR"]
-        WSO = (params.TDWI * (1 - FR)) * FO
-        DWSO = 0.
-        TWSO = WSO + DWSO
-        # Initial Pod Area Index
-        PAI = WSO * params.SPA
-
-        self.states = self.StateVariables(kiosk, publish=["TWSO", "WSO", "PAI"],
-                                          WSO=WSO, DWSO=DWSO, TWSO=TWSO,
-                                          PAI=PAI)
+    # ---------------------------------------------------------------------------
 
     @prepare_rates
     def calc_rates(self, day, drv):
-        rates = self.rates
-        states = self.states
-        params = self.params
+        # set the daytime temperature as rate variable
+        r = self.rates
+        r.DAYTEMP = drv.DTEMP
 
-        FO = self.kiosk["FO"]
-        ADMI = self.kiosk["ADMI"]
-        TRANSL = self.kiosk["TRANSL"]
-
-        rates.GRSO = ADMI * FO
-        rates.DRSO = 0.0
-        rates.GWSO = rates.GRSO - rates.DRSO + TRANSL
-
-        if params.ISINK == 1:
-            NUMGR = self.kiosk["NUMGR"]
-            if NUMGR > 0.:
-                # Low temperature reduction factor for the
-                # potential grain formation PGRIG
-                FTLOW = params.TMGTB(rates.DAYTEMP)
-
-                # potential growth of the grains(GWSO_PT)
-                GWSO_PT = NUMGR * FTLOW * params.PGRIG
-
-                # maximal grain mass growth (GWSO_MX)
-                GWSO_MX = max(0.0, NUMGR * params.GRMX - states.WSO)
-                # grain  growth limited by both maximal grain mass(GWSO_MX) and by
-                # potential growth of the grains(GWSO_PT)
-                GWSO_SK = min(GWSO_MX, GWSO_PT)
-
-                # source or sink limitation
-                rates.GWSO = min(rates.GWSO, GWSO_SK)
-                if GWSO_SK < rates.GWSO:
-                    # change translocation rate in case of sink limitation;
-                    rates.TRANSL = -(rates.GWSO - GWSO_SK) + TRANSL
-
+    # ---------------------------------------------------------------------------
     @prepare_states
     def integrate(self, day):
-        params = self.params
-        rates = self.rates
-        states = self.states
+        """Updates the state variable and checks for phenologic stages
+        """
 
-        # Stem biomass (living, dead, total)
-        states.WSO += rates.GWSO
-        states.DWSO += rates.DRSO
-        states.TWSO = states.WSO + states.DWSO
+        p = self.params
+        r = self.rates
+        s = self.states
 
-        # Calculate Pod Area Index (SAI)
-        states.PAI = states.WSO * params.SPA
+        DVS = self.kiosk["DVS"]
+        TWST = self.kiosk["TWST"]
+        TWLV = self.kiosk["TWLV"]
+
+        number_of_sinks = s.NUMGR
+
+        if DVS >= 1.0 and s.TAGBSF <= 0.:
+            # establish total leaf and stem dry weight at anthesis
+            s.TAGBSF = TWLV + TWST
+
+        # Calculate the number of sinks as a function of temperature sensitivity
+        # and establish if DVS is in the heat sesnsitive period
+        if s.HSTAGE == "not sensitive":
+            if DVS >= p.DVSHEB:
+                self._next_stage(day)
+
+        elif s.HSTAGE == "temp sensitive":
+            s.CMDTEMP += r.DAYTEMP
+            if DVS >= p.DVSHEF:
+                self._next_stage(day)
+
+                # sink reduction factor due to low temperature
+                # FTEMP_LOW = p.TMGTB(r.DAYTEMP)
+
+                # number of sinks (per ha) as determined by total leaf and stem
+                # dry weight at anthesis
+                number_of_sinks = self._number_of_sinks()
+
+
+        elif s.HSTAGE == "end temp sensitivity":
+            dummy = None
+
+        else:  # Problem no heat stage defined
+            msg = "No HSTAGE defined in the heat stress around flowering module."
+            raise exc.PCSEError(msg)
+
+        s.NUMGR = number_of_sinks
+
+    # ---------------------------------------------------------------------------
+    def _next_stage(self, day):
+        """Moves states.STAGE to the next temp sensitivity stage"""
+        s = self.states
+
+        current_HSTAGE = s.HSTAGE
+        if s.HSTAGE == "not sensitive":
+            s.HSTAGE = "temp sensitive"
+            s.DSB = day
+
+        elif s.HSTAGE == "temp sensitive":
+            s.HSTAGE = "end temp sensitivity"
+            s.DSE = day
+
+        else:  # Problem no heat stage defined
+            msg = "No HSTAGE defined in temp sensitivity around flowering module."
+            raise exc.PCSEError(msg)
+
+        msg = "Changed temp sensivity hstage '%s' to '%s' on %s"
+        self.logger.info(msg % (current_HSTAGE, s.HSTAGE, day))
+
+    # ---------------------------------------------------------------------------
+    def _number_of_sinks(self):
+        # determine number of sinks as defined by heat stress around anthesis and
+        # total weight (death+alive) at anthesis
+        p = self.params
+        s = self.states
+
+        if p.IHEAT == 1:
+            delta = s.DSE - s.DSB
+            try:
+                MDTEMP = s.CMDTEMP / float(delta.days)
+            except ZeroDivisionError:
+                msg = "Could not calculate heatstress. No days counted"
+                raise exc.PCSEError(msg)
+            FHSTRESS = p.RDGRTB(MDTEMP)
+        else:
+            FHSTRESS = 1.0
+
+        return FHSTRESS * FHSTRESS * (p.NUMGA + p.NUMGB * s.TAGBSF)
--- pcse/crop/storage_organ_dynamics.py
+++ pcse/crop/storage_organ_dynamics.py
@@ -4,35 +4,37 @@
 # Adapted for translocation, sink limitation and
 # heat stress by Iwan Supit (iwan.supit@wur.nl), June 2016
 
-from ..traitlets import Float, Int, Instance
+from ..traitlets import Float, AfgenTrait
 from ..decorators import prepare_rates, prepare_states
-from ..util import limit
 from ..base_classes import ParamTemplate, StatesTemplate, RatesTemplate, \
     SimulationObject, VariableKiosk
 
+
 class WOFOST_Storage_Organ_Dynamics(SimulationObject):
     """Implementation of storage organ dynamics.
-    
+
     Storage organs are the most simple component of the plant in WOFOST and
     consist of a static pool of biomass. Growth of the storage organs is the
     result of assimilate partitioning. Death of storage organs is not
     implemented and the corresponding rate variable (DRSO) is always set to
     zero.
-    
+
     Pods are green elements of the plant canopy and can as such contribute
     to the total photosynthetic active area. This is expressed as the Pod
     Area Index which is obtained by multiplying pod biomass with a fixed
     Specific Pod Area (SPA).
 
     **Simulation parameters**
-    
+
     =======  ============================================= =======  ============
      Name     Description                                   Type     Unit
     =======  ============================================= =======  ============
     TDWI     Initial total crop dry weight                  SCr      |kg ha-1|
     SPA      Specific Pod Area                              SCr      |ha kg-1|
-    GRMX     Maximal grain mass                             SCr      |kg|
-    PGRIG    Potential grain formation                      SCr      |kg|
+    GRMX     Maximal grain mass one grain                   SCr      |kg|
+    PGRIG    Potential grain formation per grain            SCr      |kg|
+    TMGTB    Reduction factor of grain formation  as
+             function. Values from SWHEAT model
     =======  ============================================= =======  ============
 
     **State variables**
@@ -56,35 +58,36 @@ class WOFOST_Storage_Organ_Dynamics(SimulationObject):
     GWSO     Net change in storage organ biomass                N   |kg ha-1 d-1|
     TRANSL   Weight that is translocated to storage organs      N   |kg ha-1 d-1|
     =======  ================================================= ==== ============
-    
+
     **Signals send or handled**
-    
+
     None
-    
+
     **External dependencies**
-    
+
     =======  =================================== =================  ============
      Name     Description                         Provided by         Unit
     =======  =================================== =================  ============
     ADMI     Above-ground dry matter             CropSimulation     |kg ha-1 d-1|
              increase
-    FO       Fraction biomass to storage organs  DVS_Partitioning    - 
-    FR       Fraction biomass to roots           DVS_Partitioning    - 
+    FO       Fraction biomass to storage organs  DVS_Partitioning    -
+    FR       Fraction biomass to roots           DVS_Partitioning    -
     =======  =================================== =================  ============
     """
 
-    class Parameters(ParamTemplate):      
-        SPA   = Float(-99.)
-        TDWI  = Float(-99.)
+    class Parameters(ParamTemplate):
+        SPA = Float(-99.)
+        TDWI = Float(-99.)
         ISINK = Float(-99.)
         PGRIG = Float(-99.)
-        GRMX  = Float(-99.)
+        GRMX = Float(-99.)
+        TMGTB = AfgenTrait()  # Low temperature response function for grain formation as a function of day-time temp.
 
     class StateVariables(StatesTemplate):
-        WSO  = Float(-99.) # Weight living storage organs
-        DWSO = Float(-99.) # Weight dead storage organs
-        TWSO = Float(-99.) # Total weight storage organs
-        PAI  = Float(-99.) # Pod Area Index
+        WSO = Float(-99.)  # Weight living storage organs
+        DWSO = Float(-99.)  # Weight dead storage organs
+        TWSO = Float(-99.)  # Total weight storage organs
+        PAI = Float(-99.)  # Pod Area Index
 
     class RateVariables(RatesTemplate):
         GRSO = Float(-99.)
@@ -100,62 +103,58 @@ class WOFOST_Storage_Organ_Dynamics(SimulationObject):
         """
 
         self.params = self.Parameters(parvalues)
-        self.rates  = self.RateVariables(kiosk)
+        self.rates = self.RateVariables(kiosk)
         self.kiosk = kiosk
-        
+
         # INITIAL STATES
         params = self.params
         # Initial storage organ biomass
         FO = self.kiosk["FO"]
         FR = self.kiosk["FR"]
-        WSO  = (params.TDWI * (1-FR)) * FO
+        WSO = (params.TDWI * (1 - FR)) * FO
         DWSO = 0.
         TWSO = WSO + DWSO
         # Initial Pod Area Index
         PAI = WSO * params.SPA
 
-        self.states = self.StateVariables(kiosk, publish=["TWSO","WSO","PAI"],
+        self.states = self.StateVariables(kiosk, publish=["TWSO", "WSO", "PAI"],
                                           WSO=WSO, DWSO=DWSO, TWSO=TWSO,
                                           PAI=PAI)
 
     @prepare_rates
     def calc_rates(self, day, drv):
-        rates  = self.rates
+        rates = self.rates
         states = self.states
         params = self.params
-        
+
         FO = self.kiosk["FO"]
         ADMI = self.kiosk["ADMI"]
         TRANSL = self.kiosk["TRANSL"]
-
 
         rates.GRSO = ADMI * FO
         rates.DRSO = 0.0
         rates.GWSO = rates.GRSO - rates.DRSO + TRANSL
 
-        if params.ISINK ==1:
-            NUMGR  = self.kiosk["NUMGR"]
+        if params.ISINK == 1:
+            # set the source growth rate of the storage organs
+            GWSO_SR = rates.GWSO
+            NUMGR = self.kiosk["NUMGR"]
             if NUMGR > 0.:
                 # Low temperature reduction factor for the
                 # potential grain formation PGRIG
-                FTLOW = params.TMGTB(rates.DAYTEMP)
-
-                # potential growth of the grains(GWSO_PT)
+                FTLOW = params.TMGTB(drv.DTEMP)
+                # potential growth of the grains GWSO_PT
                 GWSO_PT = NUMGR * FTLOW * params.PGRIG
-
-                # maximal grain mass growth (GWSO_MX)
+                # maximal grain mass growth GWSO_MX
                 GWSO_MX = max(0.0, NUMGR * params.GRMX - states.WSO)
-                # grain  growth limited by both maximal grain mass(GWSO_MX) and by
-                # potential growth of the grains(GWSO_PT)
+                # grain  growth limited by both maximal grain mass GWSO_MX and by
+                # potential growth of the grains GWSO_PT
                 GWSO_SK = min(GWSO_MX, GWSO_PT)
-
                 # source or sink limitation
-                rates.GWSO = min(rates.GWSO, GWSO_SK)
-                if GWSO_SK < rates.GWSO:
+                rates.GWSO = min(rates.GWSO_SR, GWSO_SK)
+                if GWSO_SK < GWSO_SR:
                     # change translocation rate in case of sink limitation;
                     rates.TRANSL = -(rates.GWSO - GWSO_SK) + TRANSL
-
-
 
     @prepare_states
     def integrate(self, day):
@@ -170,3 +169,9 @@ class WOFOST_Storage_Organ_Dynamics(SimulationObject):
 
         # Calculate Pod Area Index (SAI)
         states.PAI = states.WSO * params.SPA
+
+
+
+
+
+
