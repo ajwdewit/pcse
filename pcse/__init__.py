b0443730f72ca8852b7482b3271a01d2edc6661b
Allard de Wit <allard.dewit@wur.nl>
2017-08-20 21:53:13 +0200

    bumped version to 5.3.1

--- pcse/__init__.py
+++ pcse/__init__.py
@@ -29,7 +29,7 @@ See Also
 from __future__ import print_function
 __author__ = "Allard de Wit <allard.dewit@wur.nl>"
 __license__ = "European Union Public License"
-__version__ = "5.3.0"
+__version__ = "5.3.1"
 __stable__ = False
 
 import sys, os
