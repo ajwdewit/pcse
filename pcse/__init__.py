632cbd80a2f978d9b485b2091b46fb94c0de68e2
Wit, Allard de <allard.dewit@wur.nl>
2021-09-20 17:27:00 +0200

    updated to 5.5.0 stable

--- pcse/__init__.py
+++ pcse/__init__.py
@@ -29,8 +29,8 @@ See Also
 from __future__ import print_function
 __author__ = "Allard de Wit <allard.dewit@wur.nl>"
 __license__ = "European Union Public License"
-__stable__ = False
-__version__ = "5.5.0-dev"
+__stable__ = True
+__version__ = "5.5.0"
 
 import sys, os
 from . import util
