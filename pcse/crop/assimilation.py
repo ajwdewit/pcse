d77c4fa46181be5116c54373d03cb24c596360eb
Herman Berghuijs <herman.berghuijs@wur.nl>
2022-03-01 10:58:12 +0100

    totass2() added as copy of totass(). Wofost_Assimilation2() calls totass2() instead of totass()

--- pcse/crop/assimilation.py
+++ pcse/crop/assimilation.py
@@ -72,6 +72,59 @@ def totass(DAYL, AMAX, EFF, LAI, KDIF, AVRAD, DIFPP, DSINBE, SINLD, COSLD):
 
     return DTGA
 
+def totass2(DAYL, AMAX, EFF, LAI, KDIF, AVRAD, DIFPP, DSINBE, SINLD, COSLD):
+    """ This routine calculates the daily total gross CO2 assimilation by
+    performing a Gaussian integration over time. At three different times of
+    the day, irradiance is computed and used to calculate the instantaneous
+    canopy assimilation, whereafter integration takes place. More information
+    on this routine is given by Spitters et al. (1988).
+
+    FORMAL PARAMETERS:  (I=input,O=output,C=control,IN=init,T=time)
+    name   type meaning                                    units  class
+    ----   ---- -------                                    -----  -----
+    DAYL    R4  Astronomical daylength (base = 0 degrees)     h      I
+    AMAX    R4  Assimilation rate at light saturation      kg CO2/   I
+                                                          ha leaf/h
+    EFF     R4  Initial light use efficiency              kg CO2/J/  I
+                                                          ha/h m2 s
+    LAI     R4  Leaf area index                             ha/ha    I
+    KDIF    R4  Extinction coefficient for diffuse light             I
+    AVRAD   R4  Daily shortwave radiation                  J m-2 d-1 I
+    DIFPP   R4  Diffuse irradiation perpendicular to direction of
+                light                                      J m-2 s-1 I
+    DSINBE  R4  Daily total of effective solar height         s      I
+    SINLD   R4  Seasonal offset of sine of solar height       -      I
+    COSLD   R4  Amplitude of sine of solar height             -      I
+    DTGA    R4  Daily total gross assimilation           kg CO2/ha/d O
+
+    Authors: Daniel van Kraalingen
+    Date   : April 1991
+
+    Python version:
+    Authors: Allard de Wit
+    Date   : September 2011
+    """
+
+    # Gauss points and weights
+    XGAUSS = [0.1127017, 0.5000000, 0.8872983]
+    WGAUSS = [0.2777778, 0.4444444, 0.2777778]
+
+    # calculation of assimilation is done only when it will not be zero
+    # (AMAX >0, LAI >0, DAYL >0)
+    DTGA = 0.
+    if (AMAX > 0. and LAI > 0. and DAYL > 0.):
+        for i in range(3):
+            HOUR   = 12.0+0.5*DAYL*XGAUSS[i]
+            SINB   = max(0.,SINLD+COSLD*cos(2.*pi*(HOUR+12.)/24.))
+            PAR    = 0.5*AVRAD*SINB*(1.+0.4*SINB)/DSINBE
+            PARDIF = min(PAR,SINB*DIFPP)
+            PARDIR = PAR-PARDIF
+            FGROS = assim(AMAX,EFF,LAI,KDIF,SINB,PARDIR,PARDIF)
+            DTGA += FGROS*WGAUSS[i]
+    DTGA *= DAYL
+
+    return DTGA
+
 
 def assim(AMAX, EFF, LAI, KDIF, SINB, PARDIR, PARDIF):
     """This routine calculates the gross CO2 assimilation rate of
@@ -393,7 +446,7 @@ class WOFOST_Assimilation2(SimulationObject):
         AMAX *= p.TMPFTB(drv.DTEMP)
         KDIF = p.KDIFTB(DVS)
         EFF  = p.EFFTB(drv.DTEMP) * p.CO2EFFTB(p.CO2)
-        DTGA = totass(DAYL, AMAX, EFF, LAI, KDIF, drv.IRRAD, DIFPP, DSINBE, SINLD, COSLD)
+        DTGA = totass2(DAYL, AMAX, EFF, LAI, KDIF, drv.IRRAD, DIFPP, DSINBE, SINLD, COSLD)
 
         # correction for low minimum temperature potential
         DTGA *= p.TMNFTB(TMINRA)
