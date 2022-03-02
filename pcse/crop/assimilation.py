55bc3cbc6b38effc9b88d076ddd51424718371c0
Herman Berghuijs <herman.berghuijs@wur.nl>
2022-03-02 16:18:05 +0100

    Obsolete parameters removed from assimilation.

--- pcse/crop/assimilation.py
+++ pcse/crop/assimilation.py
@@ -72,7 +72,7 @@ def totass(DAYL, AMAX, EFF, LAI, KDIF, AVRAD, DIFPP, DSINBE, SINLD, COSLD):
 
     return DTGA
 
-def totass2(DAYL, DVS, AMAXTB, AMAX_INTERCEPT, AMAX_SLOPE,  CO2AMAX, TMPF, EFF, KN, LAI, NLV, KDIF, AVRAD, DIFPP, DSINBE, SINLD, COSLD):
+def totass2(DAYL, CO2AMAX, TMPF, EFF, KN, LAI, NLV, KDIF, AVRAD, DIFPP, DSINBE, SINLD, COSLD):
     """ This routine calculates the daily total gross CO2 assimilation by
     performing a Gaussian integration over time. At three different times of
     the day, irradiance is computed and used to calculate the instantaneous
@@ -118,7 +118,7 @@ def totass2(DAYL, DVS, AMAXTB, AMAX_INTERCEPT, AMAX_SLOPE,  CO2AMAX, TMPF, EFF, 
             PAR    = 0.5*AVRAD*SINB*(1.+0.4*SINB)/DSINBE
             PARDIF = min(PAR,SINB*DIFPP)
             PARDIR = PAR-PARDIF
-            FGROS = assim2(DVS, AMAXTB, AMAX_INTERCEPT, AMAX_SLOPE, CO2AMAX, TMPF, EFF, KN, LAI, NLV, KDIF, SINB, PARDIR, PARDIF)
+            FGROS = assim2(CO2AMAX, TMPF, EFF, KN, LAI, NLV, KDIF, SINB, PARDIR, PARDIF)
             DTGA += FGROS*WGAUSS[i]
     DTGA *= DAYL
 
@@ -189,7 +189,7 @@ def assim(AMAX, EFF, LAI, KDIF, SINB, PARDIR, PARDIF):
     FGROS  = FGROS*LAI
     return FGROS
 
-def assim2(DVS, AMAXTB, AMAX_INTERCEPT, AMAX_SLOPE, CO2AMAX, TMPF, EFF, KN, LAI, NLV, KDIF, SINB, PARDIR, PARDIF):
+def assim2(CO2AMAX, TMPF, EFF, KN, LAI, NLV, KDIF, SINB, PARDIR, PARDIF):
     """This routine calculates the gross CO2 assimilation rate of
     the whole crop, FGROS, by performing a Gaussian integration
     over depth in the crop canopy. At three different depths in
@@ -512,10 +512,6 @@ class WOFOST_Assimilation2(SimulationObject):
         LAI = k.LAI
         NLV = k.NamountLV
 
-        # Parameter values used to calculate AMAX from SLN
-        AMAX_SLOPE = p.AMAX_SLOPE
-        AMAX_INTERCEPT = p.AMAX_INTERCEPT
-
         # 7-day running average of TMIN
         self._TMNSAV.appendleft(drv.TMIN)
         TMINRA = sum(self._TMNSAV)/len(self._TMNSAV)
@@ -535,7 +531,7 @@ class WOFOST_Assimilation2(SimulationObject):
         CO2EFFTB = p.CO2EFFTB(p.CO2)        
         EFF  = p.EFFTB(drv.DTEMP) * CO2EFFTB
 
-        DTGA = totass2(DAYL, DVS, p.AMAXTB, p.AMAX_INTERCEPT, p.AMAX_SLOPE, CO2AMAX, TMPF, EFF, p.KN, LAI, NLV, KDIF, drv.IRRAD, DIFPP, DSINBE, SINLD, COSLD)
+        DTGA = totass2(DAYL, CO2AMAX, TMPF, EFF, p.KN, LAI, NLV, KDIF, drv.IRRAD, DIFPP, DSINBE, SINLD, COSLD)
 
         # correction for low minimum temperature potential
         DTGA *= p.TMNFTB(TMINRA)
