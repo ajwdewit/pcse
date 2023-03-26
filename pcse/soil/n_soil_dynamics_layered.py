4ed2c1d29510bc38496958211d3eb7f002924966
Herman Berghuijs <herman.berghuijs@wur.nl>
2023-03-26 14:30:19 +0200

    RAGE_am is now calculated by ON_APPLY_N

--- pcse/soil/n_soil_dynamics_layered.py
+++ pcse/soil/n_soil_dynamics_layered.py
@@ -20,6 +20,7 @@ class N_soil_dynamics_layered(SimulationObject):
     # Placeholders
     _RNO3AM = None
     _RNH4AM = None
+    _RAGEAM = None
     _RORGMATAM = None
     _RCORGAM = None
     _RNORGAM = None
@@ -63,6 +64,7 @@ class N_soil_dynamics_layered(SimulationObject):
         RCORG = Instance(np.ndarray)
         RNORG = Instance(np.ndarray)
 
+        RAGEAM = Instance(np.ndarray)
         RORGMATAM = Instance(np.ndarray)
         RCORGAM = Instance(np.ndarray)
         RNORGAM = Instance(np.ndarray)
@@ -149,6 +151,7 @@ class N_soil_dynamics_layered(SimulationObject):
                   "ORGMATT": ORGMATT,  "CORGT": CORGT, "NORGT": NORGT, "RMINT": RMINT, "NH4T": NH4T, "NO3T": NO3T, 
                   "NH4LEACHCUM": NH4LEACHCUM, "NO3LEACHCUM": NO3LEACHCUM, "NDENITCUM": NDENITCUM, "NLOSSCUM": NLOSSCUM}
         #self.states = self.StateVariables(kiosk, publish=["NAVAIL", "ORGMAT", "CORG", "NORG", "ORGMATT", "CORGT", "NORGT"], **states)
+        self._RAGEAM = np.zeros_like(AGE)
         self._RORGMATAM = np.zeros_like(ORGMAT)
         self._RCORGAM = np.zeros_like(CORG)
         self._RNORGAM = np.zeros_like(NORG)
@@ -193,11 +196,13 @@ class N_soil_dynamics_layered(SimulationObject):
                     r.RORGMAT[am,il] = 0.
                     r.RCORG[am,il] = 0.
                     r.RNORG[am,il] = 0.
-
+        
+        r.RAGEAM = self._RAGEAM
         r.ORGMATAM = self._RORGMATAM
         r.CORGAM = self._RCORGAM
         r.NORGAM = self._RNORGAM
 
+        self._RAGEAM = np.zeros_like(s.AGE)
         self._RORGMATAM = np.zeros_like(r.ORGMATAM)
         self._RCORGAM = np.zeros_like(r.CORGAM)
         self._RNORGAM = np.zeros_like(r.NORGAM)
@@ -305,7 +310,7 @@ class N_soil_dynamics_layered(SimulationObject):
 
         for am in range(0, r.RAGE.shape[0]):
             for il in range(0, r.RAGE.shape[1]):
-                AGE[am, il] = s.AGE[am,il] + r.RAGE[am, il] * delt
+                AGE[am, il] = s.AGE[am,il] + (r.RAGEAM[am, il] + r.RAGE[am, il]) * delt
                 ORGMAT[am, il] = s.ORGMAT[am,il] + (r.RORGMAT[am, il] + r.ORGMATAM[am, il]) * delt 
                 CORG[am, il] = s.CORG[am,il] + (r.RCORG[am, il] + r.CORGAM[am, il]) * delt 
                 NORG[am, il] = s.NORG[am, il] + (r.RNORG[am, il] + r.NORGAM[am, il]) * delt 
@@ -364,11 +369,11 @@ class N_soil_dynamics_layered(SimulationObject):
         sni = self.SoilNNitrateModel()
         sonm = self.SoilOrganicNModel()
 
-        AGE_am = np.zeros((1, len(self.soiln_profile)))
-        AGE0_am =  np.zeros_like(AGE_am)
-        RORGMAT_am = np.zeros_like(AGE_am)
-        RCORG_am =  np.zeros_like(AGE_am)
-        RNORG_am =  np.zeros_like(AGE_am)
+        RAGE_am = np.zeros((1, len(self.soiln_profile)))
+        AGE0_am =  np.zeros_like(RAGE_am)
+        RORGMAT_am = np.zeros_like(RAGE_am)
+        RCORG_am =  np.zeros_like(RAGE_am)
+        RNORG_am =  np.zeros_like(RAGE_am)
         RNH4_am = np.zeros_like(s.NH4)
         RNO3_am = np.zeros_like(s.NO3)
 
@@ -378,7 +383,7 @@ class N_soil_dynamics_layered(SimulationObject):
         for il, layer in enumerate(self.soiln_profile):
             zmax = zmin + self.soiln_profile[il].Thickness
             AGE0_am[0,il] = initial_age * self.y_to_d
-            AGE_am[0,il] = initial_age * self.y_to_d            
+            RAGE_am[0,il] = initial_age * self.y_to_d            
             RORGMAT_am[0, il] = sonm.calculate_organic_material_application_amount(amount, application_depth, f_orgmat, layer.Thickness, zmax, zmin) * self.m2_to_ha
             RCORG_am[0, il] =  sonm.calculate_organic_carbon_application_amount(minip_C, amount, application_depth, f_orgmat, layer.Thickness, zmax, zmin) * self.m2_to_ha
             RNORG_am[0, il] = sonm.calculate_organic_nitrogen_application_amount(minip_C, amount, application_depth, cnratio, f_orgmat, layer.Thickness, zmax, zmin) * self.m2_to_ha
@@ -386,21 +391,24 @@ class N_soil_dynamics_layered(SimulationObject):
             RNO3_am[il] = sni.calculate_NO3_application_amount(amount, application_depth, f_NO3, layer.Thickness, zmax, zmin) * self.m2_to_ha / layer.Thickness_m
             zmin = zmax
 
-        AGE = np.concatenate((s.AGE, AGE_am), axis = 0)
+        #RAGE = np.concatenate((s.ORGMAT, np.zeros((1, len(self.soiln_profile)))), axis = 0)
         AGE0 = np.concatenate((s.AGE0, AGE0_am), axis = 0)
         s.ORGMAT = np.concatenate((s.ORGMAT, np.zeros((1, len(self.soiln_profile)))), axis = 0)
         s.CORG = np.concatenate((s.CORG, np.zeros((1, len(self.soiln_profile)))), axis = 0)
         s.NORG = np.concatenate((s.NORG, np.zeros((1, len(self.soiln_profile)))), axis = 0)
+        s.AGE = np.concatenate((s.AGE, np.zeros((1, len(self.soiln_profile)))), axis = 0)
+        RAGEAM = np.concatenate((self._RAGEAM, RAGE_am), axis = 0)
         RORGMATAM = np.concatenate((self._RORGMATAM, RORGMAT_am), axis = 0)
         RCORGAM = np.concatenate((self._RCORGAM, RCORG_am), axis = 0)
         RNORGAM = np.concatenate(( self._RNORGAM, RNORG_am), axis = 0)
         #NH4 = s.NH4 + NH4_am * delt
         #NO3 = s.NO3 + NO3_am * delt
         s.AGE0 = AGE0
-        s.AGE = AGE
+        #s.AGE = AGE
         #s.ORGMAT = ORGMAT
         #s.CORG = CORG
         #s.NORG = NORG
+        self._RAGEAM = RAGEAM
         self._RORGMATAM = RORGMATAM
         self._RCORGAM = RCORGAM
         self._RNORGAM = RNORGAM
