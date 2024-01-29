4ef02c78ad54372073fa68876a95984a0c685d07
Wit, Allard de <allard.dewit@wur.nl>
2024-01-29 13:19:12 +0100

    Changed version to Stable

--- pcse/__init__.py
+++ pcse/__init__.py
@@ -29,7 +29,7 @@ See Also
 from __future__ import print_function
 __author__ = "Allard de Wit <allard.dewit@wur.nl>"
 __license__ = "European Union Public License"
-__stable__ = False
+__stable__ = True
 __version__ = "5.5.6"
 
 
