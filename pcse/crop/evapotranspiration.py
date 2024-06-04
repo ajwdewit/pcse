448ed6c969af0f8afe8246e36afd0636404bd744
Allard de Wit <ajwdewit@xs4all.nl>
2024-06-04 12:32:28 +0200

    updated evapotranspiration
    - Herman solved a bug in the evapotranspiration module for oxygen stress in layered soils
    - delete wofost8.py

--- pcse/crop/evapotranspiration.py
+++ pcse/crop/evapotranspiration.py
@@ -495,6 +495,7 @@ class EvapotranspirationCO2Layered(SimulationObject):
     # stress (IDWST, IDOST)
     _IDWST = Int(0)
     _IDOST = Int(0)
+    _DSOS = Int(0)
 
     soil_profile = None
 
@@ -576,14 +577,14 @@ class EvapotranspirationCO2Layered(SimulationObject):
             # for non-rice crops, and possibly deficient land drainage
             RFOS[i] = 1.
             if p.IAIRDU == 0 and p.IOX == 1:
-                RFOSMX = limit(0., 1., (p.SM0 - k.SM)/p.CRAIRC)
-                # maximum reduction reached after 4 days
-                RFOS[i] = RFOSMX + (1. - min(k.DSOS, 4)/4.)*(1.-RFOSMX)
-
+                SMAIR = layer.SM0 - layer.CRAIRC
+                if(SM >= SMAIR):
+                    self._DSOS = min((self._DSOS + 1), 4)
+                else:
+                    self._DSOS = 0                
+                RFOSMX = limit(0., 1., (layer.SM0 - SM)/layer.CRAIRC)
+                RFOS[i] = RFOSMX + (1. - min( self._DSOS, 4)/4.)*(1.-RFOSMX)
             root_fraction = max(0.0, (min(k.RD, depth + layer.Thickness) - depth)) / k.RD
-
-            # Max transpiration rate multiplied with reduction factors for oxygen and water,
-            # multiplied by the root fraction
             RFTRA_layer = RFOS[i] * RFWS[i]
             TRALY[i] = r.TRAMX * RFTRA_layer * root_fraction
 
