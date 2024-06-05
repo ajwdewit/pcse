6543f0f026c61309e8f654bf33fc0993bae85cbe
Wit, Allard de <allard.dewit@wur.nl>
2024-06-05 08:28:05 +0200

    updates to requirements.txt for readthedocs

--- pcse/__init__.py
+++ pcse/__init__.py
@@ -29,7 +29,7 @@ from __future__ import print_function
 __author__ = "Allard de Wit <allard.dewit@wur.nl>"
 __license__ = "European Union Public License"
 __stable__ = False
-__version__ = "6.0.3"
+__version__ = "6.0.4"
 
 
 import sys, os
