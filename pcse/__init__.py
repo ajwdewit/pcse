f4cfbc7aa1904dc5df0826425ffb98e23fa88ee9
Allard de Wit <ajwdewit@xs4all.nl>
2025-09-22 14:17:23 +0200

    Delete  DB subpackage
    - removed the DB subpackage to get rid of the sqlalchemy dependency
    - moved the demo db dump to tests/test_data

--- pcse/__init__.py
+++ pcse/__init__.py
@@ -72,7 +72,7 @@ import logging.config
 from .settings import settings
 logging.config.dictConfig(settings.LOG_CONFIG)
 
-from . import db
+#from . import db
 from . import input
 from . import tests
 from . import agromanager
@@ -85,7 +85,7 @@ pcse_db_file = os.path.join(settings.PCSE_USER_HOME, "pcse.db")
 if not os.path.exists(pcse_db_file):
     print("Building PCSE demo database at: %s ..." % pcse_db_file, end=" ")
     pcse_home = os.path.dirname(__file__)
-    pcse_db_dump_file = os.path.join(pcse_home, "db", "pcse", "pcse_dump.sql")
+    pcse_db_dump_file = os.path.join(pcse_home, "tests", "test_data", "pcse_dump.sql")
     try:
         util.load_SQLite_dump_file(pcse_db_dump_file, pcse_db_file)
         print("OK")
